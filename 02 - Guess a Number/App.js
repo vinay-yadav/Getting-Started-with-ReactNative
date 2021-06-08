b57c13674abb34680f1497357c54ca08b1983623
Vinay Yadav <yadav.vinay1508@gmail.com>
2021-06-08 23:05:55 +0530

    GuessANumber Init

---
+++ 02 - Guess a Number/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+import GameScreen from './screens/StartGameScreen';
+
+export default function App() {
+    return (
+        <View style={styles.screen}>
+            <Header title='Guess a Number' />
+            <GameScreen />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    }
+});
